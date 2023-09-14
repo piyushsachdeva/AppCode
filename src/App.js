0fa5f384b870100188a2583cbf3ec8383ffcad5d
Emil Karimov <47632277+EmilAstronaut@users.noreply.github.com>
2023-09-14 09:21:04 +0400

    Update App.js
    added new comment

--- src/App.js
+++ src/App.js
@@ -87,7 +87,7 @@ class App extends Component {
                         fontWeight: "bolder",
                     }}
                 >
-                    10WeeksofCloudOps - TODO LIST
+                    #10WeeksofCloudOps1 - TODO LIST
                 </Row>
   
                 <hr />
